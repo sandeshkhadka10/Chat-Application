73aef051b07ffe6040641e19d80de8673735e4e9
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-06 09:34:13 +0545

    created the auth middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import {User} from "../models/user.models.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import httpStatus from "http-status";
+
+dotenv.config();
+
+export const userVerification = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!req.session.user || req.session.user.token !== token) {
+    return res.status(httpStatus.UNAUTHORIZED).json({ status: false, message: "Session expired or invalid" });
+  }
+
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(httpStatus.UNAUTHORIZED).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error("Auth error:", err.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
