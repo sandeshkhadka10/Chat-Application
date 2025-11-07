e9dc2b4855e8728f9f817aa73bc3c3a06197eb87
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-07 07:58:49 +0545

    made the contextapi

---
+++ frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,136 @@
+import React, { createContext, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import httpStatus from "http-status";
+import { toast} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const AuthContext = createContext({});
+
+const client = axios.create({
+    baseURL: "http://localhost:8000/api/v1/users",
+    withCredentials:true
+});
+
+export const AuthProvider = ({ children }) => {
+    const authContext = useContext(AuthContext);
+
+    const [userData, setUserData] = useState(authContext);
+
+    const router = useNavigate();
+
+    const handleRegister = async (email, username, password) => {
+        try {
+            let request = await client.post("/register", {
+                email: email,
+                username: username,
+                password: password
+            });
+            if (request.status === httpStatus.CREATED) {
+                toast.success(request.data.message);
+                setTimeout(()=>{
+                    router("/home");
+                },2500);
+            }
+        } catch (error) {
+            toast.error(error.response?.data?.message);
+        }
+    }
+
+    const handleLogin = async (username, password) => {
+        try {
+            let request = await client.post("/login", {
+                username: username,
+                password: password
+            });
+            if (request.status === httpStatus.CREATED) {
+                setUserData({username});
+                toast.success(request.data.message);
+                setTimeout(() => {
+                    router("/home");
+                },2500);
+            }
+        } catch (error) {
+            toast.error(error.response?.data?.message);
+        }
+    }
+
+    const handleLogout = async()=>{
+        try{
+            let request = await client.get("/logout");
+            if(request.status === httpStatus.CREATED){
+                setUserData(null);
+                toast.success(request.data.message);
+                setTimeout(() => {
+                    router("/");
+                },2500); 
+            }
+        }catch(error){
+            toast.error(error.response?.data?.message);
+        }
+
+    }
+
+    const handleForgetPassword = async(email) => {
+        try{
+            let request = await client.post("/forgetPassword",{
+                email:email
+            });
+            if(request.status === httpStatus.OK){
+                toast.success(request.data.message);
+                setTimeout(()=>{
+                    router("/resetPassword");
+                },2500);
+            }
+        }catch(error){
+            toast.error(error.response?.data?.message);
+        }
+    }
+
+    const handleResetPassword = async(email,resetCode,newPassword) => {
+        try{
+            let request = await client.post("/resetPassword",{
+                email:email,
+                resetCode:resetCode,
+                newPassword:newPassword
+            });
+            if(request.status === httpStatus.OK){
+                toast.success(request.data.message);
+                setTimeout(() => {
+                  router("/auth");  
+                },2500);
+            }
+        }catch(error){
+            toast.error(error.response?.data?.message);
+        }
+    }
+
+    const addToUserHistory = async (meetingCode) => {
+        try {
+            let request = await client.post("/add_to_activity", {
+                meeting_code: meetingCode
+            });
+            return request.data;
+        } catch (error) {
+            throw error.response?.data?.message;
+        }
+    }
+
+    const getHistoryOfUser = async () => {
+        try {
+            let request = await client.get("/get_all_activity");
+            return request.data;
+        } catch (error) {
+            throw error.response?.data?.message;
+        }
+    }
+
+    const data = {
+        userData, setUserData, handleRegister, handleLogin, handleLogout, handleForgetPassword, handleResetPassword, addToUserHistory, getHistoryOfUser
+    }
+    return (
+        <AuthContext.Provider value={data}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
