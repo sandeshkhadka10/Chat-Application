eb10eab2fa84b73d0e06f6439cc255b99af7cb49
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2025-11-07 08:13:30 +0545

    working over authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import LandingPage from "./pages/Landing";
+import Authentication from "./pages/Authentication";
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,6 +9,7 @@ function App(){
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<Authentication />} />
       </Routes>
       <ToastContainer />
     </Router>
